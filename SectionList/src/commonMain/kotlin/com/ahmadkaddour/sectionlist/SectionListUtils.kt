0496c7388e98bbd01bbec4b29c759c9dd8a8d94b
Ahmad Kaddour <ahmad.kadour222@gmail.com>
2025-06-23 22:31:40 +0300

    adding code documentation to SectionListUtils.kt

--- SectionList/src/commonMain/kotlin/com/ahmadkaddour/sectionlist/SectionListUtils.kt
+++ SectionList/src/commonMain/kotlin/com/ahmadkaddour/sectionlist/SectionListUtils.kt
@@ -6,6 +6,13 @@ internal fun List<Int>.toPrefixSum(): List<Int> {
     return List(this.size) { i -> sum += this[i]; sum }
 }
 
+/**
+ * Converts an item index in the flattened list to its corresponding section index.
+ * This function uses a binary search algorithm to efficiently find the section index.
+ *
+ * @param sectionsSizePrefixSum A list representing the prefix sum of the sizes of all sections.
+ * @return The index of the section to which the item belongs.
+ */
 internal fun Int.toSectionIndex(sectionsSizePrefixSum: List<Int>): Int {
     var low = 0
     var high = sectionsSizePrefixSum.lastIndex
@@ -22,6 +29,13 @@ internal fun Int.toSectionIndex(sectionsSizePrefixSum: List<Int>): Int {
     return low
 }
 
+
+/**
+ * Converts a section index to its corresponding item index in the flattened list.
+ *
+ * @param sectionsSizePrefixSum A list representing the prefix sum of the sizes of all sections.
+ * @return The item index in the flattened list that corresponds to the start of the section.
+ */
 internal fun Int.toItemIndex(sectionsSizePrefixSum: List<Int>): Int {
-    return if (this <= 0) 0 else sectionsSizePrefixSum[this - 1] + this
+    return if (this <= 0) 0 else sectionsSizePrefixSum[this - 1] - 1
 }
