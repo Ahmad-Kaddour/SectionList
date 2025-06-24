da4281519088f5ab9a266565075231da53eecbbe
Ahmad Kaddour <ahmad.kadour222@gmail.com>
2025-06-24 17:06:12 +0300

    Add preconditions checks for `toSectionIndex` and `toItemIndex` methods

--- SectionList/src/commonMain/kotlin/com/ahmadkaddour/sectionlist/SectionListUtils.kt
+++ SectionList/src/commonMain/kotlin/com/ahmadkaddour/sectionlist/SectionListUtils.kt
@@ -12,10 +12,19 @@ internal fun List<Int>.toPrefixSum(): List<Int> {
  *
  * @param sectionsSizePrefixSum A list representing the prefix sum of the sizes of all sections.
  * @return The index of the section to which the item belongs.
- * @throws IllegalArgumentException if it's called on negative int.
+ * @throws IllegalArgumentException if `itemIndex` is negative, `sectionsSizePrefixSum` is empty,
+ *                                  or `itemIndex` is out of bounds (greater than or equal to the total number of items).
  */
 internal fun Int.toSectionIndex(sectionsSizePrefixSum: List<Int>): Int {
-    if (this < 0) throw IllegalArgumentException("Index can't be negative")
+    require(this >= 0) {
+        "Item index cannot be negative: $this"
+    }
+    require(sectionsSizePrefixSum.isNotEmpty()) {
+        "sectionsSizePrefixSum cannot be empty."
+    }
+    require(this < sectionsSizePrefixSum.last()) {
+        "Item index ($this) is out of bounds for the total number of items (${sectionsSizePrefixSum.last()})."
+    }
 
     var low = 0
     var high = sectionsSizePrefixSum.lastIndex
@@ -38,10 +47,15 @@ internal fun Int.toSectionIndex(sectionsSizePrefixSum: List<Int>): Int {
  *
  * @param sectionsSizePrefixSum A list representing the prefix sum of the sizes of all sections.
  * @return The item index in the flattened list that corresponds to the start of the section.
- * @throws IllegalArgumentException if it's called on negative int.
- */
+ * @throws IllegalArgumentException if `sectionIndex` is negative, `sectionsSizePrefixSum` is empty,
+ *                                  or `sectionIndex` is out of bounds (greater than the number of sections).
+ **/
 internal fun Int.toItemIndex(sectionsSizePrefixSum: List<Int>): Int {
-    if (this < 0) throw IllegalArgumentException("Index can't be negative")
+    require(this >= 0) { "Section index cannot be negative: $this" }
+    require(sectionsSizePrefixSum.isNotEmpty()) { "sectionsSizePrefixSum cannot be empty." }
+    require(this <= sectionsSizePrefixSum.lastIndex) {
+        "Section index ($this) is out of bounds. Max valid section index is ${sectionsSizePrefixSum.lastIndex}."
+    }
 
-    return if (this <= 0) 0 else sectionsSizePrefixSum[this - 1] - 1
+    return if (this == 0) 0 else sectionsSizePrefixSum[this - 1] - 1
 }
